1f2e75a43f8394cb12919124bc169701d03e3e92
oscarkraemer <carloscarkraemer@dhcp-eduroam-hy-77.mobile.helsinki.fi>
2014-05-12 16:59:25 +0300

    cosmetics
    Fixed indents

--- game.cpp
+++ game.cpp
@@ -1,7 +1,6 @@
 #include "game.h"
 
-void Game::startGame(int size)
-{
+void Game::startGame(int size){
 	this->length= size;
 	newBoard(this->length);
 
@@ -22,13 +21,9 @@ void Game::startGame(int size)
 	while(gameEnd==false);
 }
 
-void Game::newBoard(int size)
-{
-
-	for (int i=0; i<size; i++)
-	{
-		for (int j=0; j<size; j++)
-		{
+void Game::newBoard(int size){
+	for (int i=0; i<size; i++){
+		for (int j=0; j<size; j++){
 			this->board[i][j]=0;
 		}
 	}
@@ -42,8 +37,7 @@ void Game::newBoard(int size)
 	insertNewNumber();
 }
 
-int Game::returnRandomPlate()
-{
+int Game::returnRandomPlate(){
 	//TODO: Create a better random
 	usleep(500000);
 	srand (time(NULL));
@@ -52,50 +46,34 @@ int Game::returnRandomPlate()
 	return  num;
 }
 
-void Game::PrintBoard()
-{
-	for (int i=0; i<this->length; i++)
-	{
-		for (int j=0; j<this->length; j++)
-		{
+void Game::PrintBoard(){
+	for (int i=0; i<this->length; i++){
+		for (int j=0; j<this->length; j++){
 			std::cout << "| " << this->board[i][j] << " " ;
 		}
 		std::cout << "|" << std::endl;
-
 	}
 	std::cout << "--------------------------------" << std::endl;
-
-
 }
 
-void Game::insertNewNumber()
-{
+//TODO: Are there a for each element in an array?
+void Game::insertNewNumber(){
 	srand (time(NULL));
 	int count = 0;
-	for (int i=0; i<this->length; i++)
-	{
-		for (int j=0; j<this->length; j++)
-		{
-
-			if (this->board[i][j] == 0)
-			{
+	for (int i=0; i<this->length; i++){
+		for (int j=0; j<this->length; j++){
+			if (this->board[i][j] == 0){
 				count++;
 			}
 		}
 	}
-
 	count = rand() % count +1;
 	//std::cout << count << std::endl;
-
-	for (int i=0; i<this->length; i++)
-	{
-		for (int j=0; j<this->length; j++)
-		{
-			if (this->board[i][j] == 0)
-			{
+	for (int i=0; i<this->length; i++){
+		for (int j=0; j<this->length; j++){
+			if (this->board[i][j] == 0){
 				count--;
-				if (count == 0)
-				{
+				if (count == 0){
 					this->board[i][j] = returnRandomPlate();
 				}
 			}
@@ -103,132 +81,96 @@ void Game::insertNewNumber()
 	}
 }
 
-bool Game::checkIfGameOver()
-{
+bool Game::checkIfGameOver(){
 	int count = 0;
 	for (int i=0; i<this->length; i++)
 	{
 		for (int j=0; j<this->length; j++)
 		{
-
-			if (this->board[i][j] == 0)
-			{
+			if (this->board[i][j] == 0){
 				count++;
 			}
 		}
 	}
-	if (count == (this->length*this->length))
-	{
+	if (count == (this->length*this->length)){
 		return true;
 	}
 	return false;
 }
 
-void Game::moveDirection(std::string direct)
-{
+void Game::moveDirection(std::string direct){
 	int legt = this->length;
-
 	//Horizontal +1 ==left, -1 ==right, 0== doesn't move
-	for(int i = 0; i < legt; i++)
-	{
-		for (int j = 0; j < legt; j++)
-		{
-			for (int k = j+1; k < legt; k++)
-			{
+	for(int i = 0; i < legt; i++){
+		for (int j = 0; j < legt; j++){
+			for (int k = j+1; k < legt; k++){
 				bool contin =false;
-				if(direct=="left")
-				{
+				if(direct=="left"){
 					contin = executeMove(i,j,i,k);
-				}
-				else if(direct=="right")
-				{
+				}else if(direct=="right"){
 					contin = executeMove(i,legt-1-j,i,legt-1-k);
-				}
-				else if(direct=="up")
-				{
+				}else if(direct=="up"){
 					contin = executeMove(j,i,k,i);
-				}
-
-				else if(direct=="down")
-				{
+				}else if(direct=="down"){
 					contin = executeMove(legt-1-j, i, legt-1-k, i);
 				}
-
 				if(contin==false)
-				{
-					break;
-				}
+				break;
 			}
 		}
 	}
 }
 
 //true==continue false==break
-bool Game::executeMove(int start_x , int start_y, int check_x , int check_y)
-{
-
-	if (this->board[check_x][check_y] == 0)
-	{
+bool Game::executeMove(int start_x , int start_y, int check_x , int check_y){
+	if (this->board[check_x][check_y] == 0) {
 		return true;
-	}
-	else if (this->board[start_x][start_y]==this->board[check_x][check_y])
-	{
+	}else if (this->board[start_x][start_y]==this->board[check_x][check_y]){
 		this->board[start_x][start_y] = 2*this->board[check_x][check_y];
 		this->board[check_x][check_y] = 0;
 		return false;
-	}
-	else if (this->board[start_x][start_y]==0)
-	{
+	}else if (this->board[start_x][start_y]==0){
 		this->board[start_x][start_y] = this->board[check_x][check_y];
 		this->board[check_x][check_y] = 0;
 		return false;
-	}
-	else if (this->board[start_x][start_y] != this->board[check_x][check_y])
-	{
+	}else if (this->board[start_x][start_y] != this->board[check_x][check_y]) {
 		return false;
 	}
 	return true;
 }
 
-string Game::input()
-{
+string Game::input(){
 	string toReturn = "false";
   	cout << "Press up, down, left, right or q for quit" << endl; 	// Output prompt 
   	do{
-  		system("stty raw");   // Set terminal to raw mode 
-  		char input = getchar();// Wait for single character 
-
+  		system("stty raw");   	// Set terminal to raw mode 
+  		char input = getchar();	// Wait for single character 
   		//cout << "-->" << input << "<--int->" << int(input) <<endl;
-
   		if (input == 'q'){
   			toReturn = "quit";
   		}
   		//TODO: There should be a correct way to do this
-  		if (int(input)== 27) // If possible arrow key
-  		{
+  		if (int(input)== 27){ 	// If possible arrow key
   			input = getchar();
   			input = getchar();
-  			switch (int(input))
-  			{
-  				case 65:
-  					toReturn = "up";
-  					break;
-  				case 66:
-  					toReturn = "down";
-  					break;
-  				case 67:
-  					toReturn = "right";
-  					break;
-  				case 68:
-  					toReturn = "left";
-  					break;
-
-  			}
-
+  			switch (int(input)){
+			case 65:
+				toReturn = "up";
+				break;
+			case 66:
+				toReturn = "down";
+				break;
+			case 67:
+				toReturn = "right";
+				break;
+			case 68:
+				toReturn = "left";
+				break;
+			}
   		}
   	}while (toReturn=="false");
   
-  	system("stty cooked"); // Reset terminal to normal "cooked" mode 
+  	system("stty cooked"); 	// Reset terminal to normal "cooked" mode 
   	return toReturn;
 }
 
